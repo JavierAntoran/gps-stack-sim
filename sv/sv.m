952012383362f89c4b9087756df8d061bffb8f10
Alberto Mur <565825@unizar.es>
2017-04-13 21:04:06 +0200

    Set fixed time and location of ephemeris

--- sv/sv.m
+++ sv/sv.m
@@ -4,11 +4,14 @@ clear all
 ROOTDIR = fileparts(get_lib_path);
 
 almFile = strcat(ROOTDIR,'/files/Almanac/W918.alm');
-ephFile = strcat(ROOTDIR,'/files/RINEX/brdc0920.17n');
+ephFile = strcat(ROOTDIR,'/files/ephemeris/brdc0920.17n');
 
 [r_eph, r_head] = read_rinex_nav(ephFile, 32);
 
-[ satp, orbit_parameters ] = rinex2ecef(r_head, r_eph);
+[~, gps_sec] = cal2gpstime([2017 04 04 16 51 30]);
+gps_sec = gps_sec+r_head.leapSeconds;
+
+[ satp, orbit_parameters ] = rinex2ecef(r_head, r_eph, 86400);
 
 image_file = fullfile(ROOTDIR,'/sv/land_ocean_ice_2048.png');
 
