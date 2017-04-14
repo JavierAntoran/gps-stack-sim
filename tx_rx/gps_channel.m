0eae7a503da4beeb903a7eef4d69c12885a5905b
Javier Antoran Cabiscol <javir803@gmail.com>
2017-04-14 11:49:29 +0200

    renamed folder

--- tx_rx/gps_channel.m
+++
@@ -1,8 +0,0 @@
-function [] = gps_channel(file, time, Rpos, sCA, L)
-
-[ R_c_offset, R_rel_offset, R_iono, R_trop ] = gen_channel( head, eph, time, Rpos );
-[ delayVec ] = propagation_delay( head, eph, time, Rpos );
-
-t_delay = delayVec + R_c_offset + R_rel_offset + R_iono + R_trop;
-
-end
