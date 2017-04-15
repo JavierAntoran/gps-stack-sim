4701a35399ab1c833db7a4b107dabd4e3d5a178c
Javier Antoran Cabiscol <javir803@gmail.com>
2017-04-15 19:34:15 +0200

    more plotting scripts

--- lib/txlib/gps_channel.m
+++ lib/txlib/gps_channel.m
@@ -1,4 +1,4 @@
-function [delay_CA, cicles] = gps_channel(head, eph, time, Rpos, sCA, L)
+function [delay_CA, cicles, prop_delay, sat_clock_offset, sat_clock_rel, iono_T, trop_T_equiv] = gps_channel(head, eph, time, Rpos, sCA, L)
 
 % includes tropo, iono, clock error and relativistic clock
 % although the clock errors are not strictly caused by the channel
